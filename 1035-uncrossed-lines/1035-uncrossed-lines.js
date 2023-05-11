3a2f2f4465679f8c89aa7a7b92e29152ea7cceb5
Kimsu <107941176+Kimsu10@users.noreply.github.com>
2023-05-12 01:19:15 +0900

    Time: 73 ms (80.00%), Space: 44.8 MB (55.00%) - LeetHub

---
+++ 1035-uncrossed-lines/1035-uncrossed-lines.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxUncrossedLines = function(nums1, nums2) {
+  let arr = new Array(nums1.length + 1);
+    
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(nums2.length + 1).fill(0);
+  }
+  
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 1; j < arr[i].length; j++) {
+      if (nums1[i - 1] === nums2[j - 1]) {
+        arr[i][j] = arr[i - 1][j - 1] + 1;
+      } else {
+        arr[i][j] = Math.max(arr[i - 1][j], arr[i][j - 1]);
+      }
+    }
+  }
+  
+  return arr[arr.length - 1][arr[0].length - 1];
+};
