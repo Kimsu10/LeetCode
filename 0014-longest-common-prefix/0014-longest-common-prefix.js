1f323126b69c98fbb161e9ffed5f557d77bcd417
Kimsu <107941176+Kimsu10@users.noreply.github.com>
2023-05-20 23:07:27 +0900

    Time: 70 ms (24.75%), Space: 41.7 MB (93.85%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+   let pre  = strs[0];
+    for(let i = 1; i < strs.length; i++){
+        while(strs[i].indexOf(pre) !== 0 ){
+            pre = pre.substring(0, pre.length -1)
+            if( pre === ""){
+                return ""
+            }
+        }
+    }return pre
+};
